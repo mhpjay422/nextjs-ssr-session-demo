e263559551c1ee5714a8094f9b9c0b2ca99e7fd8
Jason Gong <mhpjay@gmail.com>
2022-12-13 09:00:08 -0500

    revert SSR

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,26 +2,24 @@ import { GetServerSidePropsContext, NextPage } from 'next';
 import { unstable_getServerSession } from "next-auth/next";
 import { Session } from 'next-auth';
 import { authOptions } from './api/auth/[...nextauth]';
+import { useSession } from 'next-auth/react';
 
-const Hosts: NextPage<{ session: Session }> = ({ session }) => {
-
+const Hosts: NextPage = () => {
+  const { data: session, status } = useSession();
   return (
-    <div style={{width: "100%"}}>
-      { session && 
+    <div style={{ width: "100%" }}>
+      {session && (
         <div
           style={{
-            marginLeft: "50%", 
-            marginRight: "50%", 
+            marginLeft: "50%",
+            marginRight: "50%",
             marginTop: "50vh",
-            }}>
+          }}
+        >
           Session!!!
         </div>
-      }
-      { !session && 
-        <div>
-          Missing a session
-        </div>
-      }
+      )}
+      {!session && <div>Missing a session</div>}
     </div>
   );
 };
