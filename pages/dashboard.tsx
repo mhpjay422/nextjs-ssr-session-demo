2735966b8f26d1ff5e8bcda1a1f72973ade8d71f
Jason Gong <mhpjay@gmail.com>
2022-12-13 09:07:40 -0500

    update

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,27 +2,43 @@ import { GetServerSidePropsContext, NextPage } from 'next';
 import { unstable_getServerSession } from "next-auth/next";
 import { Session } from 'next-auth';
 import { authOptions } from './api/auth/[...nextauth]';
-import { useSession } from 'next-auth/react';
 
-const Hosts: NextPage = () => {
-  const { data: session, status } = useSession();
+const Hosts: NextPage<{ session: Session }> = ({ session }) => {
+
   return (
-    <div style={{ width: "100%" }}>
-      {session && (
+    <div style={{width: "100%"}}>
+      { session && 
         <div
           style={{
-            marginLeft: "50%",
-            marginRight: "50%",
+            marginLeft: "50%", 
+            marginRight: "50%", 
             marginTop: "50vh",
-          }}
-        >
+            }}>
           Session!!!
         </div>
-      )}
-      {!session && <div>Missing a session</div>}
+      }
+      { !session && 
+        <div>
+          Missing a session
+        </div>
+      }
     </div>
   );
 };
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await unstable_getServerSession(
+                    context.req,
+                    context.res,
+                    authOptions
+                  )
+
+  return {
+    props: {
+      session
+    },
+  };
+}
 
 export default Hosts;
 
